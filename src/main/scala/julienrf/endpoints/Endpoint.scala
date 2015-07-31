b70bdbaccc0e500bea310cc288d17de1c56ab001
Julien Richard-Foy <julien@richard-foy.fr>
2015-07-31 14:22:13 +0200

    big changes. Still things to improve.

--- src/main/scala/julienrf/endpoints/Endpoint.scala
+++ src/main/scala/julienrf/endpoints/Endpoint.scala
@@ -6,20 +6,109 @@ import play.api.mvc.{Handler, RequestHeader}
 import play.api.routing.Router
 import play.twirl.api.{Html, StringInterpolation}
 
-sealed trait Endpoint {
-  def description: String
-  def method: Method
-  def path: String
+trait FlatConcat[A, B] {
+  type Out
+  def apply(a: A, b: B): Out
+  def unapply(out: Out): (A, B)
 }
-case class FixedPath(description: String, method: Method, path: String, handler: Handler) extends Endpoint {
-  def reverse: (Method, String) = (method, path)
+trait FlatConcatLowImplicits {
+  type Aux[A, B, Out0] = FlatConcat[A, B] { type Out = Out0 }
+  implicit def aB[A, B]: Aux[A, B, (A, B)] = new FlatConcat[A, B] {
+    type Out = (A, B)
+    def apply(a: A, b: B): (A, B) = (a, b)
+    def unapply(out: (A, B)): (A, B) = out
+  }
 }
-case class EndpointQS(description: String, method: Method, path: String, queryString: QueryString, handler: String => Handler) extends Endpoint {
-  def reverse(param: String): (Method, String) = (method, path ++ s"?${URLEncoder.encode(queryString.name, "UTF8")}=${URLEncoder.encode(param, "UTF8")}")
+object FlatConcat extends FlatConcatLowImplicits {
+  implicit val unitUnit: Aux[Unit, Unit, Unit] = new FlatConcat[Unit, Unit] {
+    type Out = Unit
+    def apply(a: Unit, b: Unit): Unit = Unit
+    def unapply(out: Out): (Unit, Unit) = (Unit, Unit)
+  }
+  implicit def unitA[A]: Aux[Unit, A, A] = new FlatConcat[Unit, A] {
+    type Out = A
+    def apply(a: Unit, b: A): A = b
+    def unapply(out: Out): (Unit, A) = (Unit, out)
+  }
+  implicit def AUnit[A]: Aux[A, Unit, A] = new FlatConcat[A, Unit] {
+    type Out = A
+    def apply(a: A, b: Unit): A = a
+    def unapply(out: Out): (A, Unit) = (out, Unit)
+  }
 }
 
-/** A query string parameter */
-case class QueryString(name: String, description: String)
+trait RequestCodec[A] { codecA =>
+
+  def decode(requestHeader: RequestHeader): Option[A]
+
+  def encode(a: A): MethodAndURL
+
+  def docMethod: Seq[Method]
+  def docPath: Option[String]
+  def docQS: Map[String, String]
+
+  def concat[B](codecB: RequestCodec[B])(implicit fc: FlatConcat[A, B]): RequestCodec[fc.Out] =
+    new RequestCodec[fc.Out] {
+      def decode(requestHeader: RequestHeader): Option[fc.Out] =
+        (codecA.decode(requestHeader), codecB.decode(requestHeader)) match {
+          case (Some(a), Some(b)) => Some(fc(a, b))
+          case _ => None
+        }
+      def encode(out: fc.Out): MethodAndURL = {
+        val (a, b) = fc.unapply(out)
+        val methodAndUrlA = codecA.encode(a)
+        val methodAndUrlB = codecB.encode(b)
+        MethodAndURL(
+          methodAndUrlA.method.orElse(methodAndUrlB.method),
+          methodAndUrlA.path.fold(methodAndUrlB.path)(prefix => methodAndUrlB.path.map(prefix ++ _).orElse(Some(prefix))),
+          methodAndUrlA.queryString ++ methodAndUrlB.queryString
+        )
+      }
+      val docMethod: Seq[Method] = (codecA.docMethod ++ codecB.docMethod).distinct
+      def docPath: Option[String] = codecA.docPath.fold(codecB.docPath)(prefix => codecB.docPath.map(prefix ++ _).orElse(Some(prefix)))
+      def docQS: Map[String, String] = codecA.docQS ++ codecB.docQS
+    }
+}
+
+trait RequestEncoder[A] {
+  def encode(a: A): MethodAndURL
+}
+
+case class MethodAndURL(method: Option[Method], path: Option[String], queryString: Map[String, Seq[String]]) {
+  lazy val url: String =
+    path.getOrElse("/") ++
+      queryString.flatMap { case (k, vs) => vs.map(v => URLEncoder.encode(k, "UTF8") ++ "=" ++ URLEncoder.encode(v, "UTF8")) }
+        .mkString("?", "&", "")
+}
+
+case class MethodCodec(method: Method) extends RequestCodec[Unit] {
+  def decode(requestHeader: RequestHeader): Option[Unit] =
+    if (requestHeader.method == Method.asText(method)) Some(Unit) else None
+  def encode(a: Unit): MethodAndURL = MethodAndURL(Some(method), None, Map.empty)
+  def docMethod: Seq[Method] = Seq(method)
+  def docQS: Map[String, String] = Map.empty
+  def docPath: Option[String] = None
+}
+
+case class PathCodec(path: String) extends RequestCodec[Unit] {
+  def decode(requestHeader: RequestHeader): Option[Unit] =
+    if (requestHeader.path == path) Some(Unit) else None
+  def encode(a: Unit): MethodAndURL = MethodAndURL(None, Some(path), Map.empty)
+  def docMethod: Seq[Method] = Nil
+  def docQS: Map[String, String] = Map.empty
+  def docPath: Option[String] = Some(path)
+}
+
+case class QueryStringParameterCodec(name: String, description: String) extends RequestCodec[String] {
+  def decode(requestHeader: RequestHeader): Option[String] =
+    requestHeader.getQueryString(name)
+  def encode(a: String): MethodAndURL = MethodAndURL(None, None, Map(name -> Seq(a)))
+  def docMethod: Seq[Method] = Nil
+  def docQS: Map[String, String] = Map(name -> description)
+  def docPath: Option[String] = None
+}
+
+case class Endpoint[A](description: String, codec: RequestCodec[A])(val handler: A => Handler)
 
 /** HTTP methods */
 sealed trait Method
@@ -36,31 +125,26 @@ object Method {
 
 object Endpoint {
 
-  def router(endpoints: Seq[Endpoint]): Router =
+  def router(endpoints: Seq[Endpoint[_]]): Router =
     Router.from(Function.unlift { (requestHeader: RequestHeader) =>
       endpoints
-        .collect {
-          case FixedPath(_, method, path, handler) if Method.asText(method) == requestHeader.method && path == requestHeader.path => handler
-          case EndpointQS(_, method, path, queryString, handler) if Method.asText(method) == requestHeader.method && path == requestHeader.path && requestHeader.queryString.get(queryString.name).isDefined => handler(requestHeader.queryString(queryString.name).head)
+        .collectFirst {
+          case endpoint if endpoint.codec.decode(requestHeader).nonEmpty => endpoint.handler(endpoint.codec.decode(requestHeader).get)
         }
-        .headOption
     })
 
-  def documentation(endpoint: Endpoint): Html =
-    endpoint match {
-      case FixedPath(description, method, path, _) =>
-        html"""
-          <h2>${Method.asText(method)} $path</h2>
-          <p>$description</p>
-        """
-      case EndpointQS(description, method, path, querystring, _) =>
-        html"""
-          <h2>${Method.asText(method)} $path?${querystring.name}=</h2>
-          <dl>
-            <dt>${querystring.name}</dt><dd>${querystring.description}</dd>
-          </dl>
-          <p>$description</p>
-        """
-    }
+  def documentation(endpoint: Endpoint[_]): Html =
+    html"""
+      <h2>${endpoint.codec.docMethod.map(Method.asText).mkString(" or ")} ${endpoint.codec.docPath}</h2>
+      <dl>
+        ${
+          for ((n, d) <- endpoint.codec.docQS) yield
+            html"""
+              <dt>$n</dt><dd>$d</dd>
+            """
+        }
+      </dl>
+      <p>${endpoint.description}</p>
+    """
 
 }
--- src/main/scala/julienrf/endpoints/Example.scala
+++ src/main/scala/julienrf/endpoints/Example.scala
@@ -5,31 +5,33 @@ import play.twirl.api.StringInterpolation
 
 object Example extends Controller {
 
-  lazy val foo: FixedPath =
-    FixedPath(
-      "My first endpoint", Get, "/foo",
+  lazy val index: Endpoint[Unit] =
+    Endpoint(
+      "My first endpoint", MethodCodec(Get).concat(PathCodec("/"))) { _ =>
       Action {
-        Ok(html"<a href='${foo.reverse._2}'>foo</a>, <a href='${doc.reverse._2}'>doc</a>, <a href='${hello.reverse("Julien")._2}'>Hello Julien</a>")
+        Ok(html"<a href='${index.codec.encode(()).url}'>foo</a>, <a href='${doc.codec.encode(()).url}'>doc</a>, <a href='${hello.codec.encode("Julien").url}'>Hello Julien</a>")
       }
-    )
+    }
 
-  lazy val doc: FixedPath =
-    FixedPath(
-      "Documentation", Get, "/doc",
+  lazy val doc: Endpoint[Unit] =
+    Endpoint(
+      "Documentation", MethodCodec(Get).concat(PathCodec("/doc")))( _ =>
       Action {
         Ok(html"<h1>Example Documentation</h1>${endpoints.map(Endpoint.documentation)}")
       }
     )
 
   lazy val form =
-    FixedPath("Form submission", Post, "/submit", Action(NotImplemented))
+    Endpoint("Form submission", MethodCodec(Post).concat(PathCodec("/submit")))(_ => Action(NotImplemented))
 
   lazy val hello =
-    EndpointQS(
-      "Say hello to someone", Get, "/hello", QueryString("name", "Name of the person to greet"),
-      name => Action(Ok(html"<h1>Hello $name</h1>"))
-    )
+    Endpoint(
+      "Say hello to someone",
+      MethodCodec(Get).concat(PathCodec("/hello")).concat(QueryStringParameterCodec("name", "Name of the person to greet"))
+    ) { name =>
+      Action(Ok(html"<h1>Hello $name</h1>"))
+    }
 
-  lazy val endpoints = Seq(foo, hello, doc, form)
+  lazy val endpoints = Seq(index, hello, doc, form)
 
 }
